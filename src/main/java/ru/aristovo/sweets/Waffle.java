3c8513e01c37fe6b19e8cb0183f19ab4e1d56e7f
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 13:27:55 +0300

    Создан класс Waffle (вафли), добавлен конструктор.

---
+++ src/main/java/ru/aristovo/sweets/Waffle.java
@@ -0,0 +1,12 @@
+package ru.aristovo.sweets;
+
+/**
+ * Класс "Вафля".
+ */
+
+public class Waffle extends Sweets {
+
+    public Waffle(String name, double weight, double price, String filler) {
+        super(name, weight, price, filler);
+    }
+}
