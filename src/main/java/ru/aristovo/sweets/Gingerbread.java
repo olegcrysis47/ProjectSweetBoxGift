b9e3beb706f6d1f042b142ba0e43859ff26e6904
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 13:35:52 +0300

    Создан класс Gingerbread (пряники), добавлен конструктор.

---
+++ src/main/java/ru/aristovo/sweets/Gingerbread.java
@@ -0,0 +1,12 @@
+package ru.aristovo.sweets;
+
+/**
+ * Класс "Пряник".
+ */
+
+public class Gingerbread extends Sweets {
+
+    public Gingerbread(String name, double weight, double price, String filler) {
+        super(name, weight, price, filler);
+    }
+}
