90132150b7167f9c4fd4c12cb99ee32d76622ad1
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 14:31:05 +0300

    Создан класс Marmalade (мармелада), добавлен конструктор.

---
+++ src/main/java/ru/aristovo/sweets/Marmalade.java
@@ -0,0 +1,13 @@
+package ru.aristovo.sweets;
+
+/**
+ * Класс "Мармелад".
+ */
+
+public class Marmalade extends Sweets {
+
+    public Marmalade(String name, double weight, double price, String filler) {
+        super(name, weight, price, filler);
+    }
+
+}
