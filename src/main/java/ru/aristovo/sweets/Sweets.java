d26984f5a0528ba99cbcc0571e0f62d0479969b2
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 12:35:16 +0300

    Создан абстрактный класс Sweets, проинициализированы поля, конструктор, геттеры, переопределен метод toString.

---
+++ src/main/java/ru/aristovo/sweets/Sweets.java
@@ -0,0 +1,41 @@
+package ru.aristovo.sweets;
+
+public abstract class Sweets {
+
+    protected String name;      //название сладости
+    protected double weight;    //вес, кг
+    protected double price;     //цена, руб/кг
+    protected String filler;    //добавка, начинка, наполнитель и прочее
+
+    // конструктор для создания сладости
+    public Sweets (String name, double weight, double price, String filler) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+        this.filler = filler;
+    }
+
+    //добавлены геттеры
+    public String getName() {
+        return name;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getFiller() {
+        return filler;
+    }
+
+    //переопределили вывод на экран информации и сладости
+    @Override
+    public String toString() {
+        return "Наименование: " + getName() +
+                "/tВес: " + getWeight() +
+                "/tЦена: " + getPrice() +
+                "/tДобавка: " + getFiller();
+    }
+
+}
