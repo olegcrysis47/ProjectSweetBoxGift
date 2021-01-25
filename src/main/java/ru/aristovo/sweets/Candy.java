bbcb5bb7d2617d4ea66ba20f285a1fae48e04a3c
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 12:43:52 +0300

    Создан класс Candy (конфет), добавлен конструктор.

---
+++ src/main/java/ru/aristovo/sweets/Candy.java
@@ -0,0 +1,13 @@
+package ru.aristovo.sweets;
+
+/**
+ * Класс "Конфет".
+ */
+
+public class Candy extends Sweets {
+
+    public Candy(String name, double weight, double price, String filler) {
+        super(name, weight, price, filler);
+    }
+
+}
