f64974862af360f7fffe20b334807d8d8b0399c0
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 16:22:52 +0300

    Добавлено тестирование для проверки метода removeSweet(int index) - метод удаляет сладость из коробки по индексу

--- src/main/java/ru/aristovo/Main.java
+++ src/main/java/ru/aristovo/Main.java
@@ -8,29 +8,21 @@ public class Main {
     public static void main(String[] args) {
 
         Gift gift = new Gift();
+        System.out.println("-----------------Добавляем в коробку сладости-----------------");
         gift.addSweet(new Candy("Твикс", 0.025, 70.50, "Печенье и нуга"));
-        gift.viewSweet();
-        System.out.println("------------------");
-
         gift.addSweet(new Cookies("Овсяное", 0.37, 50.15,"С орехом"));
-        gift.viewSweet();
-        System.out.println("------------------");
-
         gift.addSweet(new Waffle("Венские", 0.60, 32.5, "Со сливками"));
-        gift.viewSweet();
-        System.out.println("------------------");
-
         gift.addSweet(new Gingerbread("Медовые", 0.45, 45.11, "С джемом"));
+        gift.addSweet(new Marmalade("Мармелад", 0.23, 35.35, "С сахаром"));
+        gift.addSweet(new Candy("Гуливер", 0.05, 50.00, "Вафли"));
+        gift.addSweet(new Waffle("Венские", 0.55, 35.1, "Со сгущенкой"));
+        gift.addSweet(new Gingerbread("Клубничные", 0.51, 60.14, "С кокосовой стружкой"));
         gift.viewSweet();
-        System.out.println("------------------");
 
-        gift.addSweet(new Marmalade("Мармелад", 0.34, 43.6, "С джемом"));
-        gift.addSweet(new Marmalade("Мармелад", 0.34, 43.6, "С джемом"));
-        gift.addSweet(new Marmalade("Мармелад", 0.34, 43.6, "С джемом"));
-        gift.addSweet(new Marmalade("Мармелад", 0.34, 43.6, "С джемом"));
-        gift.addSweet(new Gingerbread("Медовые", 0.45, 45.11, "С джемом"));
+        System.out.println("-----------------Удаляем позиции-----------------");
+        gift.removeSweet(7);
         gift.viewSweet();
-        System.out.println("------------------");
+
 
     }
 
