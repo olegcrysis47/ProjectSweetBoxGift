86f66fbc6670271915271e6409a900d1071351a7
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 13:16:59 +0300

    Создан класс Cookies (печений), добавлен конструктор.

---
+++ src/main/java/ru/aristovo/sweets/Cookies.java
@@ -0,0 +1,13 @@
+package ru.aristovo.sweets;
+
+/**
+ * Класс "Печенье".
+ */
+
+public class Cookies extends Sweets {
+
+    public Cookies(String name, double weight, double price, String filler) {
+        super(name, weight, price, filler);
+    }
+
+}
