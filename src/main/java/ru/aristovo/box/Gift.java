e529f64ae5228578bc731eabc502c2623c5e9eaa
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-25 16:21:38 +0300

    Добавлена рееализация метода removeSweet(int index) для удаления позиции по индексу.

--- src/main/java/ru/aristovo/box/Gift.java
+++ src/main/java/ru/aristovo/box/Gift.java
@@ -25,6 +25,25 @@ public class Gift implements SweetBox {
     }
 
     @Override
+    public void removeSweet(int index) {
+        if (index < sweetsInGift.length) {
+            Sweets[] sw = new Sweets[sweetsInGift.length - 1];
+            for (int i = 0; i < sw.length; i++) {
+                if (i >= index) {
+                    sw[i] = sweetsInGift[i+1];
+                } else {
+                    sw[i] = sweetsInGift[i];
+                }
+            }
+            sweetsInGift = sw;
+
+
+        } else {
+            System.out.println("Нет такой позиции в коробке");
+        }
+    }
+
+    @Override
     public void viewSweet() {
         System.out.println("В коробке содержится:");
         for (Sweets sw : sweetsInGift) {
